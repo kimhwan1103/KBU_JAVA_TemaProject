6c49d4321bc724144953819c33690a201bc85473
hwan kim <rlaghks1103@gmail.com>
2021-12-06 15:59:00 +0900

    코드 정리

--- src/main/java/decodingJson.java
+++ src/main/java/decodingJson.java
@@ -50,69 +50,10 @@ public class decodingJson {
                     data.put(SkillName+"Damage", SkillDamage);
                     data.put(SkillName+"SkillSpeech", SkillSpeech);
 
-
-                    /*
-                    for(int k=0; k<arr3.size(); k++){
-                        //JSONObject tmp3 = (JSONObject)arr3.get(k);
-
-
-                        String SkillName = (String)tmp3.get("SkillName");
-                        String SkillDamage = (String)tmp3.get("damage");
-                        String SkillSpeech = (String)tmp3.get("SkillSpeech");
-
-                        data.put("SkillName", SkillName);
-                        data.put(SkillName+"Damage", SkillDamage);
-                        data.put(SkillName+"SkillSpeech", SkillSpeech);
-
-
-                    }
-                    */
-
-
                 }
 
 
             }
-            /*
-            System.out.println(arr.size());
-            for(int i=0; i<arr.size(); i++){
-                JSONObject tmp = (JSONObject)arr.get(i);
-
-                String HP = (String)tmp.get("HP");
-                String XP = (String)tmp.get("Xp");
-                String DefensivePower = (String)tmp.get("DefensivePower");
-                String damage = (String)tmp.get("damage");
-                String speech = (String)tmp.get("speech");
-
-                data.put("HP", HP);
-                data.put("XP", XP);
-                data.put("DefensivePower", DefensivePower);
-                data.put("damage", damage);
-                data.put("speech", speech);
-                JSONArray arr2 = (JSONArray)tmp.get("skill");
-
-                for(int j=0; j<arr2.size(); j++){
-                    JSONObject tmp2 = (JSONObject)arr2.get(i);
-
-                    String SkillIndex = (String)tmp2.get(Integer.toString(0));
-
-                    JSONArray arr3 = (JSONArray)tmp2.get(SkillIndex);
-
-                    for(int k=0; k<arr3.size(); k++){
-                        JSONObject tmp3 = (JSONObject)arr3.get(i);
-
-                        String SkillName = (String)tmp3.get("SkillName");
-                        String SkillDamage = (String)tmp3.get("damage");
-                        String SkillSpeech = (String)tmp3.get("SkillSpeech");
-
-                        data.put("SkillName", SkillName);
-                        data.put(SkillName+"Damage", SkillDamage);
-                        data.put(SkillName+"Speech", SkillSpeech);
-                    }
-                }
-            }
-
-             */
 
         }
         else if(SelectMod == "PlayerData"){
--- src/main/java/monsterload.java
+++ src/main/java/monsterload.java
@@ -1,44 +1,22 @@
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+
 import org.json.simple.parser.ParseException;
 
-public class monsterload {
-}
 
 //추상 팩토리 패턴으로 진행
-//interface Monster{} //몬스터 인터페이스 생성
 
 abstract class Monster{
-    //abstract Map<String, Object> LoadMonster() throws IOException, ParseException;
     public abstract String JsonDir();
 }
-
-/*
-abstract class RaidMonster implements Monster{} //몬스터를 상속하고 레이드몬스터 추상 클래스 생성
-abstract class MiniMonster implements Monster{} //몬스터를 상속하고 미니몬스터 추상 클래스 생성
-abstract class BigMonster implements Monster{} //몬스터를 상속하고 빅몬스터 추상 클래스 생성
- */
 
 class RaidMonsterFactory extends Monster{
 
     private String Dir;
 
-    /*
-        @Override
-        Map<String, Object> LoadMonster() throws IOException, ParseException {
-            JSONParser parser = new JSONParser();
-            Reader reader = new FileReader("MonsterSetting/RaidMonsterSetting.json");
-            JSONObject jsonObject = (JSONObject) parser.parse(reader);
-            return jsonObject;
-        }
-        */
     @Override
     public String JsonDir() {
         return Dir = "MonsterSetting/RaidMonsterSetting.json";
@@ -46,7 +24,6 @@ class RaidMonsterFactory extends Monster{
 }
 
 interface MonsterFactory {
-    //Monster LoadMonster();
     Monster JsonDir();
 }
 
@@ -77,12 +54,6 @@ class AbstractMonsterFactoryTest {
             MonsterData = LoadJSon.LoadJson(dir, "고블린킹", "MonsterData");
             Set<String> keys = MonsterData.keySet();
 
-            /*
-            for(String key: keys){
-                System.out.println("몬스터 세팅값" + MonsterData.get(key));
-            }
-
-             */
 
             System.out.println(MonsterData.entrySet());
 
